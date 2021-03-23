aece0bb56920ba344a0665862c827136e0e63473
abiisnn <abigail.nic.say@hotmail.com>
2021-03-22 21:34:33 -0600

    Updates

--- static/js/main.2ec259cc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),i=t(3),s=t.n(i),o=(t(8),t(9),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:"Working..."})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.2ec259cc.chunk.js.map
